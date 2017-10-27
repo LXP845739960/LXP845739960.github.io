2c18f5cfb8d7c5edb7acbe75acb3911dea2c60e5
LXP845739960 <13636604592@163.COM>
2017-10-27 08:25:13 +0800

    Site updated: 2017-10-27 08:25:12

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// 样式
+import '../css/main.scss'
+// 上报
+import './report'
+// 图片查看器
+import Viewer from './viewer'
+// 分享
+import Share from './share'
+// 边缘
+import Aside from './aside'
+
+import {addLoadEvent} from './util'
+
+addLoadEvent(function() {
+	Share.init()
+	Viewer.init()
+	Aside.init()
+})
+if($(".instagram").length) {
+        require(['/photo.js', '/fancybox/jquery.fancybox.js', '/js/jquery.lazyload.js'], function(obj) {
+            obj.init();
+        });
+    }
